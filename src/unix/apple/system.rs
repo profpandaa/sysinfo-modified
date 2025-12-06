43fafd8086e343ba4cbffd295492ade556e2a800
~panda <30387483+profpandaa@users.noreply.github.com>
2025-12-06 23:51:07 +0100

    chore: impl custom used memory func (actually now)

--- src/unix/apple/system.rs
+++ src/unix/apple/system.rs
@@ -359,6 +359,10 @@ impl SystemInner {
     pub(crate) fn used_memory(&self) -> u64 {
         self.mem_used
     }
+    
+    pub(crate) fn free_like_used_memory(&self) -> u64 {
+        self.get_used_memory()
+    }
 
     pub(crate) fn total_swap(&self) -> u64 {
         self.swap_total
