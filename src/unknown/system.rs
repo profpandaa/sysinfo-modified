0aec35aa2355a3470d6dd65f16f14324486a5f74
~panda <30387483+profpandaa@users.noreply.github.com>
2025-12-06 23:54:50 +0100

    chore: impl custom used memory func (actually now)

--- src/unknown/system.rs
+++ src/unknown/system.rs
@@ -87,6 +87,11 @@ impl SystemInner {
         0
     }
 
+    pub(crate) fn free_like_used_memory(&self) -> u64 {
+        self.used_memory()
+    }
+
+
     pub(crate) fn total_swap(&self) -> u64 {
         0
     }
